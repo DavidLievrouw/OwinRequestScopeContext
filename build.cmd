38faaaf2dcf04684bb894dbfe8be349488c315ba
David Lievrouw <david.lievrouw@recomatics.com>
2019-04-29 09:53:18 +0200

    Attempt to fix appveyor script.

--- build.cmd
+++ build.cmd
@@ -2,6 +2,6 @@
 cls
 SET DIR=%~dp0%
 SET SRCDIR=./src/
-dotnet publish %SRCDIR%OwinRequestScopeContext/OwinRequestScopeContext.csproj --framework netstandard2.0 --verbosity detailed /p:IsPublishing="true"
-dotnet publish %SRCDIR%OwinRequestScopeContext/OwinRequestScopeContext.csproj --framework net462 --verbosity detailed /p:IsPublishing="true"
+dotnet publish %SRCDIR%OwinRequestScopeContext/OwinRequestScopeContext.csproj --framework netstandard2.0 --configuration Release --verbosity detailed /p:IsPublishing="true"
+dotnet publish %SRCDIR%OwinRequestScopeContext/OwinRequestScopeContext.csproj --framework net462 --configuration Release --verbosity detailed /p:IsPublishing="true"
 pause
