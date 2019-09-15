130640cae81e9e9375fd6f6821731cf38ae25644
David Lievrouw <david.lievrouw@recomatics.com>
2019-09-15 22:25:03 +0200

    Make test.cmd run tests for all supported target frameworks, and bring verbosity down.

--- test.cmd
+++ test.cmd
@@ -1,5 +1,6 @@
 @echo off
 cls
 SET SRCDIR=./src/
-dotnet test %SRCDIR%OwinRequestScopeContext.Tests/OwinRequestScopeContext.Tests.csproj --verbosity detailed
+dotnet test %SRCDIR%OwinRequestScopeContext.Tests/OwinRequestScopeContext.Tests.csproj --framework net461
+dotnet test %SRCDIR%OwinRequestScopeContext.Tests/OwinRequestScopeContext.Tests.csproj --framework netcoreapp2.2
 pause
