fb8e7379cbc79ad28f5ceb8ad0031b13e1436224
David Lievrouw <david.lievrouw@recomatics.com>
2019-09-15 18:21:16 +0200

    Remove traces of net462 target and replace by net461. Breaking change, so bumping the major version.

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@ cls
 SET DIR=%~dp0%
 SET SRCDIR=./src/
 dotnet publish %SRCDIR%OwinRequestScopeContext/OwinRequestScopeContext.csproj --framework netstandard2.0 --configuration Release --verbosity detailed --output "bin/Publish/netstandard2.0" /p:IsPublishing="true"
-dotnet publish %SRCDIR%OwinRequestScopeContext/OwinRequestScopeContext.csproj --framework net462 --configuration Release --verbosity detailed --output "bin/Publish/net462" /p:IsPublishing="true"
+dotnet publish %SRCDIR%OwinRequestScopeContext/OwinRequestScopeContext.csproj --framework net461 --configuration Release --verbosity detailed --output "bin/Publish/net461" /p:IsPublishing="true"
 pause
